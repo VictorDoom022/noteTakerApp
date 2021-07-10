8d2694116949686c56c0e0f872789582123d82bb
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-10 21:53:01 +0800

    display unarchive note

--- app/src/main/java/com/example/notetaker/ArchivedNote.java
+++ app/src/main/java/com/example/notetaker/ArchivedNote.java
@@ -6,23 +6,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.example.notetaker.adapter.DBController;
+import com.example.notetaker.adapter.NoteListAdapter;
+import com.example.notetaker.model.Note;
 import com.google.android.material.navigation.NavigationView;
 
-public class ArchivedNote extends AppCompatActivity {
+import java.util.List;
+
+public class ArchivedNote extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    Spinner spinnerSortTitle;
+    RecyclerView noteListRecyclerView;
+
+    List<Note> noteList;
+    String[] sortItemChoice = {"Title Asc", "Title Desc", "Date Asc", "Date Desc"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archived_note);
-
+        spinnerSortTitle = findViewById(R.id.sortTitleSpinner);
         Toolbar toolbar = findViewById(R.id.appBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Archive Note");
@@ -50,6 +68,60 @@ public class ArchivedNote extends AppCompatActivity {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open_drawer, R.string.navigation_close_drawer);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        spinnerSortTitle.setOnItemSelectedListener(this);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, sortItemChoice);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerSortTitle.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        DBController dbController = new DBController(getApplicationContext());
+
+        noteListRecyclerView = findViewById(R.id.noteListRecyclerView);
+        noteListRecyclerView.setHasFixedSize(true);
+        noteListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String selectedItem = adapterView.getItemAtPosition(i).toString();
+
+        if(selectedItem == sortItemChoice[0]){
+            noteList = dbController.getNoteByType(1,1);
+        }else if(selectedItem == sortItemChoice[1]){
+            noteList = dbController.getNoteByType(2,1);
+        }else if(selectedItem == sortItemChoice[2]){
+            noteList = dbController.getNoteByType(3,1);
+        }else if(selectedItem == sortItemChoice[3]){
+            noteList = dbController.getNoteByType(4,1);
+        }
+
+        NoteListAdapter adapter = new NoteListAdapter(ArchivedNote.this, noteList);
+        noteListRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // do nothing
+    }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.layout.menu_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.aboutAppItemButton){
+            Intent intent = new Intent(getApplicationContext(), AboutAppActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
